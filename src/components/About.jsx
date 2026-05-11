13f8251e4fc8d410bc28092d5f0ef09318a72efd
jishnujs123 <64188119+jishnujs123@users.noreply.github.com>
2026-05-11 21:42:56 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,7 +12,7 @@ const About = () => {
 It all started when my father sent an interest to her matrimony profile. One day, while casually checking his phone at home, I noticed an email saying that she had accepted the request.
 Being a true Malayalee detective, I immediately searched for her on Instagram and sent her a follow request. That’s where our first conversation began.
 We started talking, getting to know each other, and slowly realized that our vibes matched really well. Soon, we felt this could turn into something serious and decided to share it with our families.
-Like most Kerala families, the first reaction was not exactly a straight “yes” — mainly because of the classic “What will people think?” situation 😂
+Like most Kerala families, the first reaction was not exactly a straight “yes”, mainly because of the classic “What will people think?” situation 😂
 But eventually, both families understood us, accepted our decision, and started talking to each other. And well… that’s how this beautiful journey happened.
 There’s also a beautiful coincidence in our story, we both studied at Younus College of Engineering and Technology, but in different batches. I joined in 2012 and passed out in 2016, while she joined in 2017 and completed her studies in 2021. Same college, different timelines… maybe fate was just waiting for the right moment ❤️
           </p>
