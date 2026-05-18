d71617fb4676be0b59727a072bd4fa45726ecd4c
jishnujs123 <64188119+jishnujs123@users.noreply.github.com>
2026-05-18 14:48:04 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,13 +8,15 @@ const About = () => {
         <div className="text-center mb-14 animate-riseIn">
           <h2 className="section-title">Our Story</h2>
           <p className="section-subtitle">
-            Two families, two hearts, and one beautiful journey that begins with gratitude and joy.
-It all started when my father sent an interest to her matrimony profile. One day, while casually checking his phone at home, I noticed an email saying that she had accepted the request.
-Being a true Malayalee detective, I immediately searched for her on Instagram and sent her a follow request. That’s where our first conversation began.
-We started talking, getting to know each other, and slowly realized that our vibes matched really well. Soon, we felt this could turn into something serious and decided to share it with our families.
-Like most Kerala families, the first reaction was not exactly a straight “yes”, mainly because of the classic “What will people think?” situation 😂
-But eventually, both families understood us, accepted our decision, and started talking to each other. And well… that’s how this beautiful journey happened.
-There’s also a beautiful coincidence in our story, we both studied at Younus College of Engineering and Technology, but in different batches. I joined in 2012 and passed out in 2016, while she joined in 2017 and completed her studies in 2021. Same college, different timelines… maybe fate was just waiting for the right moment ❤️
+
+Two families, two hearts, and one beautiful journey that began with gratitude and joy. It all started when my father sent an interest to her matrimonial profile. One day, while casually checking his phone at home, I noticed an email saying that she had accepted the request. Being a true Malayalee detective, I immediately searched for her on Instagram and sent her a follow request. That’s where our first conversation began.
+<p>
+We started talking, getting to know each other, and slowly realized that our vibes matched really well. Soon, we felt that this could turn into something serious and decided to share it with our families. Like many Kerala families, the first reaction wasn’t exactly a straight “yes,” mainly because of the classic “What will people think?” situation 😂
+</p>
+But eventually, both families understood us, accepted our decision, and started talking to each other. And well… that’s how this beautiful journey began.
+<p>
+There’s also a lovely coincidence in our story. We both studied at Younus College of Engineering and Technology, but in different batches. I joined in 2012 and graduated in 2016, while she joined in 2017 and completed her studies in 2021. Same college, different timelines… maybe fate was simply waiting for the right moment ❤️
+</p>
           </p>
         </div>
 
